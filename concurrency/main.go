6c00954a54c4692ba49718fb1281bf29b87b8161
MTomys <michalltomys@protonmail.ch>
2023-06-10 18:22:43 +0200

    add single-producer-singleconsumer pattern

--- concurrency/main.go
+++ concurrency/main.go
@@ -20,19 +20,26 @@ func main() {
 
 	wg.Add(1)
 
-	go func(validOrderCh <-chan order) {
-		select {
-		case order := <-validOrderCh:
-			fmt.Printf("Valid order received: %v\n", order)
-		case order := <-invalidOrderCh:
-			fmt.Printf("Invalid order received: %v with error %v\n", order.order, order.err)
+	go func(validOrderCh <-chan order, invalidOrderCh <-chan invalidOrder) {
+	loop:
+		for {
+			select {
+			case order, ok := <-validOrderCh:
+				if ok {
+					fmt.Printf("Valid order received: %v\n", order)
+				} else {
+					break loop
+				}
+			case invalidOrder, ok := <-invalidOrderCh:
+				if ok {
+					fmt.Printf("Invalid order received: %v with error %v\n", invalidOrder.order, invalidOrder.err)
+				} else {
+					break loop
+				}
+			}
 		}
 		wg.Done()
-	}(validOrderCh)
-
-	go func(invalidOrderCh <-chan invalidOrder) {
-		wg.Done()
-	}(invalidOrderCh)
+	}(validOrderCh, invalidOrderCh)
 
 	wg.Wait()
 }
@@ -47,19 +54,23 @@ func receiveOrders(out chan<- order) {
 		}
 		out <- newOrder
 	}
+	close(out)
 }
 
 func validateOrder(in <-chan order, out chan<- order, errCh chan<- invalidOrder) {
-	order := <-in
-	if order.Quantity <= 0 {
-		errCh <- invalidOrder{order: order, err: errors.New("Quantity can not be less or equal to 0")}
-	} else {
-		out <- order
+	for order := range in {
+		if order.Quantity <= 0 {
+			errCh <- invalidOrder{order: order, err: errors.New("Quantity can not be less or equal to 0")}
+		} else {
+			out <- order
+		}
 	}
+	close(out)
+	close(errCh)
 }
 
 var rawOrders = []string{
-	`{"productCode": 1111, "quantity": -1, "status": 1}`,
+	`{"productCode": 1111, "quantity": 1, "status": 1}`,
 	`{"productCode": 1234, "quantity": 2, "status": 1}`,
 	`{"productCode": 4321, "quantity": 9, "status": 1}`,
 	`{"productCode": 4321, "quantity": -2, "status": 1}`,
